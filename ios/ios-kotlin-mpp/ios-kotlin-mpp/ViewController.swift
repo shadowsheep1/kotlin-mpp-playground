1d6ad2aee96cbfa2ebe309a37ac99ce7cbd5da03
Fabio Bombardi <shadowsheep1@users.noreply.github.com>
2020-02-05 17:01:28 +0100

    Add ios project

---
+++ ios/ios-kotlin-mpp/ios-kotlin-mpp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-kotlin-mpp
+//
+//  Created by Fabio Bombardi on 05/02/2020.
+//  Copyright © 2020 Fabio Bombardi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
