e85e623a86fcfecba5a4e57bbc1e9eebd37e6cb2
Fabio Bombardi <shadowsheep1@users.noreply.github.com>
2020-02-06 09:49:49 +0100

    on Android side all works fine.

--- kotlin-mpp/src/androidTest/kotlin/foo/MockTest.kt
+++ kotlin-mpp/src/androidTest/kotlin/foo/MockTest.kt
@@ -1,10 +1,28 @@
 package foo
 
 import org.junit.Test
+import sample.Platform
+import sample.Sample
+import sample.myClass2
 
 class MockTest {
     @Test
     fun foo() {
         assert(true)
+    }
+
+    @Test
+    fun giveMeTheAnswer() {
+        println("The ultimate answer is: ${Sample().checkMe()}")
+    }
+
+    @Test
+    fun testMyClass2() {
+        println("MyClass2 global val test: ${myClass2.printHerName()}")
+    }
+
+    @Test
+    fun testPlatform() {
+        println("Platform test: ${Platform.name}")
     }
 }
